41950c394d516cb22676bf908ed250804b04e533
Damien Montastier <montastier.damien@gmail.com>
2019-03-25 20:21:44 +0100

    init nuxt.js

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'momo-romo',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
+    ],
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }],
+  },
+  css: [{
+    src: '~/assets/scss/main.scss',
+    lang: 'scss'
+  }],
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+    }]
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** Run ESLint on save
+     */
+    // extend(config, {
+    //   isDev,
+    //   isClient
+    // }) {
+    //   if (isDev && isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
+  }
+}
