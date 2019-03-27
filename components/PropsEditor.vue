c2a0dfdf65531e72ef32f5f5aef18e2454494b86
Clément Roche <rchclement@gmail.com>
2019-03-27 19:03:52 +0100

    start editor

---
+++ components/PropsEditor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="props-editor" ref="props-editor" draggable="false"></div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  mounted() {
+    console.log(this.atlases);
+  },
+  computed: {
+    ...mapState({
+      atlases: state => state.editor.atlases
+    })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#props-editor {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 300px;
+  height: 100vh;
+  background: white;
+  z-index: 2;
+}
+</style>
