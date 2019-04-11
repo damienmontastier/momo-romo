2f195201939571619944857c119d8ca9cbd1802b
Clément Roche <rchclement@gmail.com>
2019-04-11 19:12:26 +0200

    Create Socket.js

---
+++ assets/js/utils/Socket.js
@@ -0,0 +1,38 @@
+import io from "socket.io-client";
+
+const url = "https://blooming-garden-58138.herokuapp.com/";
+
+function ID() {
+  return (
+    "_" +
+    Math.random()
+      .toString(36)
+      .substr(2, 9)
+  );
+}
+
+export default class Socket {
+  constructor() {
+    this.ID = "TESTID";
+    this.socket = io(url, {
+      query: {
+        type: "desktop",
+      },
+    });
+    this.addEvent();
+    this.createRoom();
+  }
+
+  createRoom() {
+    this.socket.emit("create room", {
+      id: this.ID,
+      type: "desktop",
+    });
+  }
+
+  addEvent() {
+    this.socket.on("time", t => {
+      // console.log(t);
+    });
+  }
+}
