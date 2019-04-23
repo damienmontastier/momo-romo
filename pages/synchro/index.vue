507c40244062fd0a79f2e0c8d4d71ac57cf682d9
Clément Roche <rchclement@gmail.com>
2019-04-23 17:53:02 +0200

    synchro OK

--- pages/synchro/index.vue
+++ pages/synchro/index.vue
@@ -1,25 +1,58 @@
 <template>
-    <div>
-        synchro
-    </div>
+  <div>
+    <div>synchro : {{isSynchro}}</div>
+    <!-- <canvas ref="qrcode"></canvas> -->
+    <div ref="qrcode" id="qrcode"></div>
+  </div>
 </template>
 
 <script>
-import { mapMutations, mapActions } from 'vuex';
+import { mapMutations, mapActions, mapState } from "vuex";
+import QRCode from "qrcode";
 export default {
-    mounted() {
-        this.init({device: this.$device.isMobile,roomID: this.$device.isMobile ? this.$route.query.id : null})
-    },
-    methods: {
-        ...mapActions({
-            'init':'synchro/init'
-        }),
-        ...mapMutations({
-        })
-    },
-}
+  mounted() {
+    this.init({
+      device: this.$device.isMobile,
+      roomID: this.$device.isMobile ? this.$route.query.id : null
+    });
+  },
+  methods: {
+    ...mapActions({
+      init: "synchro/init"
+    }),
+    ...mapMutations({}),
+    generateQRCode() {
+      console.log(this.roomID);
+      QRCode.toString(this.url + this.roomID, (error, string) => {
+        this.$refs.qrcode.innerHTML = string;
+        // if (error) console.error(error);
+        // console.log("success!");
+      });
+    }
+  },
+  computed: {
+    ...mapState({
+      isSynchro: state => state.synchro.isSynchro,
+      roomID: state => state.synchro.roomID,
+      url: state => state.synchro.url
+    })
+  },
+  watch: {
+    roomID() {
+      this.generateQRCode();
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+#qrcode {
+  height: 150px;
+  width: 150px;
 
+  svg {
+    height: 100%;
+    width: 100%;
+  }
+}
 </style>
--- store/synchro.js
+++ store/synchro.js
@@ -1,11 +1,15 @@
 import io from "socket.io-client";
 
 const serverURL = 'https://blooming-garden-58138.herokuapp.com/'
+const URL = 'http://localhost:3000/synchro/?id='
 
 export const state = () => ({
     socket: null,
     device: null,
-    roomID: null
+    roomID: null,
+    isSynchro: false,
+    serverURL: serverURL,
+    url: URL
 })
 
 export const getters = {
@@ -13,26 +17,45 @@ export const getters = {
 }
 
 export const mutations = {
-    init(state,roomID) {
-        state.socket = io(serverURL, {query:{device:state.device, roomID: roomID}});
-        state.socket.on('room created',(id)=>{
-            console.log('room created','http://localhost:3000/synchro/?id=' + id)
+    init(state, roomID) {
+        state.socket = io(serverURL, {
+            query: {
+                device: state.device,
+                roomID: roomID
+            }
+        });
+        state.socket.on('room created', (id) => {
+            console.log('room created', 'http://localhost:3000/synchro/?id=' + id)
             state.roomID = id
         })
 
-        state.socket.on('room joined',(id)=>{
-            console.log('room joined',id)
+        state.socket.on('room joined', (id) => {
+            console.log('room joined', id)
             state.roomID = id
         })
+
+        state.socket.on('synchro', (value) => {
+            console.log('SYNCHRO', value)
+            state.isSynchro = value
+        })
+
+        state.socket.on('debug', (v) => {
+            console.log(v)
+        })
     },
-    setDevice(state,device) {
+    setDevice(state, device) {
         state.device = device ? 'mobile' : 'desktop';
     }
 }
 
 export const actions = {
-    async init({commit},{device,roomID}) {
-        commit('setDevice',device);
-        commit('init',roomID);
+    async init({
+        commit
+    }, {
+        device,
+        roomID
+    }) {
+        commit('setDevice', device);
+        commit('init', roomID);
     }
 }
