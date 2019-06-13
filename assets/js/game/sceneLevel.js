c3401fc0cc871a9316770f9044de13c44049069d
damienmontastier <montastier.damien@gmail.com>
2019-06-13 19:29:42 +0200

    fix romo scale and body sprite

--- assets/js/game/sceneLevel.js
+++ assets/js/game/sceneLevel.js
@@ -1,12 +1,11 @@
 import * as THREE from 'three';
-// import OrbitControls from 'orbit-controls-es6';
+import OrbitControls from 'orbit-controls-es6';
 import FixedProp from '../objects/FixedProp'
 import CANNON from 'cannon'
 import physicParams from '../physics/physicParams';
 import Characters from '../objects/Characters';
 import cannonDebugRenderer from '../physics/CannonDebugRenderer'
 import AnimatedProp from "@/assets/js/objects/AnimatedProp";
-import Sprite from "@/assets/js/objects/Sprite";
 
 import {
     TweenMax,
@@ -52,15 +51,12 @@ export default class Level {
 
         this.clock = new THREE.Clock()
 
-
         new Characters(store).then((characters) => {
             this.characters = characters
             this.momo = this.characters.momo
             this.romo = this.characters.romo
             this.romo.scale.set(2, 2, 2)
-            this.romo.position.z = 2.5
-            this.romo.position.y = 1
-            this.romo.position.x = this.momo.position.x
+          
             this.addCharacter()
         })
 
@@ -276,127 +272,13 @@ export default class Level {
 
         this.masks = new THREE.Mesh(singleGeometry, material);
 
-        // let materialbis = new THREE.MeshBasicMaterial({
-        //     color: 0xff0000, // TODO video a mapper sur ce material
-        //     // wireframe: true,
-        //     side: THREE.DoubleSide
-        // });
+        let materialbis = new THREE.MeshBasicMaterial({
+            color: 0xff0000, // TODO video a mapper sur ce material
+            // wireframe: true,
+            side: THREE.DoubleSide
+        });
 
-        this.materialbis = new THREE.ShaderMaterial({
-            uniforms: {
-                iTime: {
-                    value: 0
-                }
-            },
-            vertexShader: `
-                varying vec3 v_position;
-                void main() {
-                    v_position = position;
-                    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-                }
-            `,
-            fragmentShader: `
-                //	Simplex 3D Noise 
-                //	by Ian McEwan, Ashima Arts
-                //
-                vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
-                vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
-                
-                float snoise(vec3 v){ 
-                const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
-                const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
-                
-                // First corner
-                vec3 i  = floor(v + dot(v, C.yyy) );
-                vec3 x0 =   v - i + dot(i, C.xxx) ;
-                
-                // Other corners
-                vec3 g = step(x0.yzx, x0.xyz);
-                vec3 l = 1.0 - g;
-                vec3 i1 = min( g.xyz, l.zxy );
-                vec3 i2 = max( g.xyz, l.zxy );
-                
-                //  x0 = x0 - 0. + 0.0 * C 
-                vec3 x1 = x0 - i1 + 1.0 * C.xxx;
-                vec3 x2 = x0 - i2 + 2.0 * C.xxx;
-                vec3 x3 = x0 - 1. + 3.0 * C.xxx;
-                
-                // Permutations
-                i = mod(i, 289.0 ); 
-                vec4 p = permute( permute( permute( 
-                            i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
-                        + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
-                        + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
-                
-                // Gradients
-                // ( N*N points uniformly over a square, mapped onto an octahedron.)
-                float n_ = 1.0/7.0; // N=7
-                vec3  ns = n_ * D.wyz - D.xzx;
-                
-                vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
-                
-                vec4 x_ = floor(j * ns.z);
-                vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
-                
-                vec4 x = x_ *ns.x + ns.yyyy;
-                vec4 y = y_ *ns.x + ns.yyyy;
-                vec4 h = 1.0 - abs(x) - abs(y);
-                
-                vec4 b0 = vec4( x.xy, y.xy );
-                vec4 b1 = vec4( x.zw, y.zw );
-                
-                vec4 s0 = floor(b0)*2.0 + 1.0;
-                vec4 s1 = floor(b1)*2.0 + 1.0;
-                vec4 sh = -step(h, vec4(0.0));
-                
-                vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
-                vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
-                
-                vec3 p0 = vec3(a0.xy,h.x);
-                vec3 p1 = vec3(a0.zw,h.y);
-                vec3 p2 = vec3(a1.xy,h.z);
-                vec3 p3 = vec3(a1.zw,h.w);
-                
-                //Normalise gradients
-                vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-                p0 *= norm.x;
-                p1 *= norm.y;
-                p2 *= norm.z;
-                p3 *= norm.w;
-                
-                // Mix final noise value
-                vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
-                m = m * m;
-                return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
-                                                dot(p2,x2), dot(p3,x3) ) );
-                }
-                
-                float map(float value, float min1, float max1, float min2, float max2) {
-                    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
-                }
-
-                varying vec3 v_position;
-                uniform float iTime;
-
-                void main() {
-                    vec3 color1 = vec3(253., 249., 235.)/255.;
-                    vec3 color2 = vec3(234., 227., 205.)/255.;
-                    color2 = vec3(214., 197., 185.)/255.;
-
-                    vec3 gradient = mix(vec3(1.0),color1,v_position.y);
-                    float noise = snoise(vec3(vec2(v_position.x * 0.1,v_position.y* 0.1* 0.5 - iTime*0.1),iTime*0.1));
-                    float grain = snoise(vec3(v_position.xy*100000.,iTime*0.1));
-                   
-                    vec3 noise_color = mix(color1,color2,clamp(noise,0.0,1.0));
-                    
-                    vec3 color = ((gradient*0.5) + grain + (noise_color * 10.5)) / 11.;
-
-                    gl_FragColor = vec4(color,.1);
-                }
-            `
-        })
-
-        var border = new THREE.Mesh(singleGeometry, this.materialbis);
+        var border = new THREE.Mesh(singleGeometry, materialbis);
         border.scale.multiplyScalar(1.01);
         this.masks.add(border);
         this.masks.scale.set(.6, .6, .6)
@@ -433,7 +315,7 @@ export default class Level {
                 setTimeout(() => {
                     this.fixedPropsGroup[i].position.x = this.camera.position.x
                     resolve(this.fixedPropsGroup[i]);
-                }, i * 100)
+                }, i * 50)
             });
 
             promises.push(promise)
@@ -441,7 +323,7 @@ export default class Level {
             promise.then((fixed) => {
                 setTimeout(() => {
                     fixed.position.copy(fixed.originPosition)
-                }, 100);
+                }, 50);
             })
         }
 
@@ -466,12 +348,10 @@ export default class Level {
 
     render() {
         // this.camera.lookAt(this.camera.position)
-        let delta = this.clock.getDelta() * 5000;
-        this.materialbis.uniforms.iTime.value+= (delta*0.0005);
 
         if (this.animatesArray.length) {
             this.animatesArray.forEach(animate => {
-                
+                const delta = this.clock.getDelta() * 5000;
                 this.time += delta;
                 animate.animate.update(delta)
                 if (this.romo.position.x >= animate.position.x - 1.5 && this.romo.position.x <= animate.position.x + 1.5) {
@@ -505,7 +385,7 @@ export default class Level {
             this.romo.position.y = Math.max(this.restrictedZone.bottom + .5, Math.min(this.restrictedZone.top, this.romo.position.y))
         }
 
-        this.cannonDebugRenderer.update()
+        // this.cannonDebugRenderer.update()
 
         if (this.characters && this.preRenderFinish) {
             this.characters.update()
